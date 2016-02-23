843852db6bf0ec7a1e72e672805d1c90b38e6105
jennabagcal <jenna.bagcal@journalism.cuny.edu>
2016-02-23 10:11:18 -0500

    Create homework.js

---
+++ homework.js
@@ -0,0 +1,18 @@
+Branch: master Find file Copy pathhomework/myjavascript.js
+bb1605d  8 days ago
+@jennabagcal jennabagcal Create myjavascript.js
+1 contributor
+RawBlameHistory     13 lines (10 sloc)  802 Bytes
+var four = 4
+var eight = 8 
+
+var addition = four + eight 
+var subtract = eight - four 
+var multiply = four * eight 
+var divide = eight / four 
+
+document.getElementById("addition").innerHTML = "When I add " + four + " pieces of sushi to " + eight + " pieces of sushi " + " I get " + addition + " pieces of sushi"; 
+document.getElementById("subtract").innerHTML = "When I subtract " + eight + " chocolate truffles from " + four + " chocolate truffles" + " I get " + subtract + " chocolate truffles";
+document.getElementById("multiply").innerHTML = "When I multiply " + four + " fish tacos by " + eight + " fish tacos I get " + multiply + " fish tacos";
+document.getElementById("divide").innerHTML = "When I divide " + eight + " baguettes by " + four + " baguettes " + " I get " + divide + " baguettes"; 
+Status API Training Shop Blog About Pricing
